2f2525f916237887878911a6c2ce54fc814d5c28
Gabrielle Kurah <gskurah@yahoo.co.uk>
2021-12-24 17:29:40 +0100

    Added Menu Fragment

---
+++ app/src/main/java/com/isep/series/DiscoverAdapter.java
@@ -0,0 +1,61 @@
+package com.isep.series;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DiscoverAdapter extends RecyclerView.Adapter<DiscoverAdapter.MyViewHolder> {
+
+    Context mContext;
+    List<Series> mData;
+
+    public DiscoverAdapter(Context mContext, List<Series> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_discover_card, parent, false);
+
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.series_photo.setImageResource(mData.get(position).getThumbnail());
+        holder.series_title.setText(mData.get(position).getTitle());
+        //TODO: MIGHT WANT TO ADD THE SEASONS STRING TO THE MODEL INSTEAD
+        holder.series_seasons.setText(mData.get(position).getSeasons());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView series_photo;
+        TextView series_title, series_seasons;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            series_photo = itemView.findViewById(R.id.discover_card_bg);
+            series_title = itemView.findViewById(R.id.discover_series_title);
+            series_seasons = itemView.findViewById(R.id.discover_series_seasons);
+        }
+    }
+}
---
+++ app/src/main/java/com/isep/series/DiscoverFragment.java
@@ -0,0 +1,65 @@
+package com.isep.series;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscoverFragment extends Fragment {
+
+    private RecyclerView discoverRV;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_discover, container, false);
+
+
+        //sets status bar to transparent
+        Window w = getActivity().getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        //RECYCLER VIEW ADAPTER SETUP
+        discoverRV = view.findViewById(R.id.rv_discover_series);
+
+        List<Series>  seriesList = new ArrayList<>();
+
+        seriesList.add(new Series("Stranger Things", R.drawable.testimg1, "2 seasons"));
+        seriesList.add(new Series("Portable Life", R.drawable.testimg2, "3 seasons"));
+        seriesList.add(new Series("Stranger Things", R.drawable.testimg2, "4 seasons"));
+        seriesList.add(new Series("Portable Life", R.drawable.testimg2, "3 seasons"));
+        seriesList.add(new Series("Portable Life", R.drawable.testimg2, "3 seasons"));
+        seriesList.add(new Series("Stranger Things", R.drawable.testimg2, "4 seasons"));
+        seriesList.add(new Series("Portable Life", R.drawable.testimg2, "3 seasons"));
+
+
+
+        DiscoverAdapter discoverAdapter = new DiscoverAdapter(view.getContext(),seriesList);
+
+        discoverRV.setAdapter(discoverAdapter);
+        discoverRV.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+
+
+
+        return view;
+    }
+
+
+}

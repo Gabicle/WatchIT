2043072d44c32283c8ec655cf1744ca2e148a2cc
Gabrielle Kurah <gskurah@yahoo.co.uk>
2021-12-28 02:56:58 +0100

    Added Login & Register Activity

---
+++ app/src/main/java/com/isep/series/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.isep.series;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/**
+ * LOGIN ACTIVITY
+ * description: login users
+ * */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
